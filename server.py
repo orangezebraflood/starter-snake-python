842bb93eca061265e62b263024da352ffcb839e7
Samantha Patrick <sam.eva.patrick@gmail.com>
2020-10-25 17:38:15 -0700

    First commit
    yayayay

--- server.py
+++ server.py
@@ -18,8 +18,8 @@ class Battlesnake(object):
         # TIP: If you open your Battlesnake URL in browser you should see this data
         return {
             "apiversion": "1",
-            "author": "",  # TODO: Your Battlesnake Username
-            "color": "#888888",  # TODO: Personalize
+            "author": "orangezebraflood",  # TODO: Your Battlesnake Username
+            "color": "#640D14",  # TODO: Personalize
             "head": "default",  # TODO: Personalize
             "tail": "default",  # TODO: Personalize
         }
