a45c9bba435f548d6e291247cd6fdc6a50e995c4
Samantha Patrick <sam.eva.patrick@gmail.com>
2020-10-25 17:54:59 -0700

    Update server.py

--- server.py
+++ server.py
@@ -43,6 +43,8 @@ class Battlesnake(object):
         # Valid moves are "up", "down", "left", or "right".
         # TODO: Use the information in cherrypy.request.json to decide your next move.
         data = cherrypy.request.json
+        data["you"] = youToken
+        print(youtToken)
 
         # Choose a random direction to move in
         possible_moves = ["up", "down", "left", "right"]
